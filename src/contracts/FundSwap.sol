c3c7cc4607253e969026101bc5c81b55bdc8620b
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-12-01 04:16:33 +0530

    CB token purchasing function written and tested

--- src/contracts/FundSwap.sol
+++ src/contracts/FundSwap.sol
@@ -4,9 +4,35 @@ import "./CbToken.sol";
 
 contract FundSwap{
     string public name = "Fund Swap";
+    uint public rate = 20;
     CbToken public cbToken;
 
     constructor (CbToken _cbToken) public{
         cbToken = _cbToken;
     }
+
+    event TokenPurchased(
+        address receiverAccount,
+        address cbToken,
+        address fundSwap,
+        uint256 amount,
+        uint rate
+    );
+
+    function buycbTokens() public payable{
+        // 1 ether = 20 CB Tokens
+        // calculating the number of CB tokens tokens to buy
+        uint256 tokens = msg.value * rate;
+
+        // checking there is enough of CB tokens to buy
+        require( cbToken.balanceOf(address(this)) >= tokens);
+
+        // transfering the tokens to the address
+        cbToken.transfer(msg.sender, tokens);
+
+        // emit and event to check and test the transfer
+        emit TokenPurchased(msg.sender,address(cbToken),address(this),tokens,rate);
+
+    }
+
 }
--- test/FundSwap.test.js
+++ test/FundSwap.test.js
@@ -9,6 +9,10 @@ function tokens(n) {
   return web3.utils.toWei(n, 'ether');
 }
 
+
+// account 0 -> owner
+// account 1 -> creator
+// account 2 -> investor
 contract('FundSwap', (accounts)=>{
 
     let cbToken, fundSwap;
@@ -16,7 +20,7 @@ contract('FundSwap', (accounts)=>{
     before(async ()=>{
 
         cbToken = await CbToken.new();
-        fundSwap = await FundSwap.deployed();
+        fundSwap = await FundSwap.new(cbToken.address);
         // transfering all the CB Token to FundSwap
         await cbToken.transfer(fundSwap.address,tokens('100000000'));
     })
@@ -32,5 +36,35 @@ contract('FundSwap', (accounts)=>{
             assert.equal(balance,tokens('100000000'));
         })
         
+    })
+
+    describe('CB token purchasing = buyTokens()', async() =>{
+
+        let result;
+        before(async()=>{
+            // token purchasing at a fixed rate
+            result = await fundSwap.buycbTokens({from: accounts[2], value: tokens('10')})
+            // console.log(result.logs[0].args.amount.toString());
+        })
+
+        it('token purchasing details', async()=>{
+
+            // checking investor balance
+            const investorBalance = await cbToken.balanceOf(accounts[2]);
+            assert.equal(investorBalance, tokens('200'));
+
+            // checking FundSwap balance
+            const fundSwapBalance = await cbToken.balanceOf(fundSwap.address);
+            assert.equal(fundSwapBalance,tokens('99999800'))
+
+            // event emitted to check all transaction details was correct
+            const event = result.logs[0].args;
+            // checking transaction details
+            assert.equal(event.receiverAccount,accounts[2]);
+            assert.equal(event.cbToken, cbToken.address);
+            assert.equal(event.fundSwap,fundSwap.address);
+            assert.equal(event.amount.toString(),tokens('200'));
+            assert.equal(event.rate.toString(),'20');
+        })
     })
 })
