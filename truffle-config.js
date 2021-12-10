63f9dccc707c74e8dc7ccf3d6523bd2b796ff6ce
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-12-10 13:09:49 +0530

    build abis directory changed

--- truffle-config.js
+++ truffle-config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
   },
   contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+  contracts_build_directory: './client/src/abis/',
   compilers: {
     solc: {
       optimizer: {
