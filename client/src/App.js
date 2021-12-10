e7f98671698785c145e72524d59cff9a35fd4356
harshshaw <harshshaw5@gmail.com>
2021-12-10 14:00:38 +0530

    added web3 support

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,72 @@
 import NewProject from "./Components/Create-Project/NewProject";
 import ExchangeDashboard from "./Components/PurchasingDashboard/ExchangeDashboard";
 import ProjectsDashboard from "./Components/ShowcaseProjects/ProjectsDashboard";
-
-import React, { useContext, useState, useEffect } from "react";
+import Web3 from 'web3';
+import React, { useState, useEffect } from "react";
+import CbToken from '../src/abis/CbToken.json';
+import FundSwap from '../src/abis/FundSwap.json';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
+
+  const [account, updateAccounts] = useState();
+  const [validate, updateValidate] = useState(false);
+  const [cbToken, updatecbToken] = useState();
+
+  useEffect(async () => {
+
+    loadWeb3();
+    loadBlockchainData();
+  }, [validate]);
+
+  const loadBlockchainData = async () => {
+    updateValidate(false);
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    const networkId = await web3.eth.net.getId();
+    updateAccounts(accounts[0]);
+    console.log("Account 0", account);
+    console.log("Network Id", networkId);
+
+    const cbTokenData = CbToken.networks[networkId];
+    if (cbTokenData) {
+      const cbToken = new web3.eth.Contract(CbToken.abi, cbTokenData.address);
+      updatecbToken(cbToken);
+      let cbTokenBalance = await cbToken.methods.balanceOf(account).call();
+      console.log("CbToken Balance==", cbTokenBalance);
+    }
+    const fundSwapData = FundSwap.networks[networkId];
+    if (fundSwapData) {
+      const fundSwap = new web3.eth.Contract(FundSwap.abi, fundSwapData.address);
+      console.log("Address===", fundSwapData.address);
+      updatecbToken(fundSwap);
+      // let fundSwapBalance = await cbToken.methods.balanceOf(fundSwapData.address).call();
+      // console.log("FundSwap Balance==", fundSwapBalance);
+    }
+  }
+
+  window.ethereum.on('accountsChanged', function (accounts) {
+    updateValidate(true);
+  })
+
+
+
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+
   return (
     <Router>
       <Routes>
